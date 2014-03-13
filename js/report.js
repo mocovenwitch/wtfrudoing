d03f6c6ac1034a563052b1678fcacd1f8a5afb61
mocovenwitch <mocovenwitch@gmail.com>
2014-03-13 16:12:37 +0800

    version 0.1.1 prepare for release

--- js/report.js
+++ js/report.js
@@ -16,7 +16,7 @@ $.fn.peity.defaults.pie = {
   width: 300
 }
 
-var MESSAGE = {
+var MESSAGE_DESCRIPTION = {
   'music': '花了 @@ &&听音乐，你真的可以好好做事吗？骗鬼把。',
   'game': '好吧，你花了 @@ &&打游戏，需要我提醒你，打游戏找不到女朋友吗……',
   'eshop': '逛网店 @@ &&，信用卡透支的滋味，好好尝尝吧。',
@@ -26,8 +26,19 @@ var MESSAGE = {
   'mocoven': '谢谢访问默考文的主页，但是用不着花 @@ &&吧',
   'search': '你在搜索什么？需要花掉 @@ &&',
   'study': '看起来好像花了 @@ &&在学习，不过效果怎么样，谁知道呢',
-  'other':'鬼知道你都乱七八糟干了些什么， @@ &&！'
+  'other':'你访问了一些乱七八糟的网站，鬼知道干了些什么，只知道你花了 @@ &&！'
 };
+
+var MESSAGE_COMMON = {
+    'RESET': '数据都清除了，让我们重头开始吧',
+    'TITLE': '看看你都干了些什么！',
+    'APP_DESCRIPTION': '有一些计划做了好多年了，比如学一种弦乐；有一些决心下了 50 遍了，比如「如果我再把学英文的时间拿去逛淘宝，'
+                       + '就！剁！手！」「如果我再把学『解剖学』的时间拿去刷微博，就不准吃三文鱼刺身！」结果呢，5 年过去了，说好的学弦乐呢？信用卡账单也把眼球吓掉了吧？'
+                       + '那你的手剁了吗？至于解剖学……你能立刻说出窦房结在哪里以及它的功能吗？<br><br>所以，好好看看，你都干了些什么吧。',
+    'UNIT': '分钟',
+    'COMMENTS': '送你一句话：亲爱的，滚去学习吧',
+    'NOTHING': '你还什么都没有干呢……'
+}
 
 var ERROR = {
   ERROR_GET_DATA_FAILED: '数据库好像坏了，没有拿到数据……重新下载应用吧……不好意思'
@@ -38,20 +49,31 @@ var ERROR = {
 var Report = {
   chart: $('span.pie'),
   init: function() {
+    $('#app_description').html(MESSAGE_COMMON['APP_DESCRIPTION']);
+    $('.title').text(MESSAGE_COMMON['TITLE']);
+    $('.comments').text(MESSAGE_COMMON['COMMENTS']);
+
     this.getData();
   },
   renderToPage: function() {
     var pieParts = '';
-    $.each(this.reportData, function(index, item) {
-      pieParts += ',' + item.time;
-    
-      var msg = MESSAGE[item.type].replace('@@', item.time).replace('&&', '分钟');
-      $('.descriptions').append('<p class=\"description-' + index + '\">' + msg + '</p>');
-    });
 
-    pieParts = pieParts.toString().substring(1, pieParts.length);
-    $('.report').find('.pie').text(pieParts);
-    this.chart.peity('pie');
+    if(!!this.reportData) {
+      $.each(this.reportData, function(index, item) {
+        pieParts += ',' + item.time;
+
+        var msg = MESSAGE_DESCRIPTION[item.type].replace('@@', item.time).replace('&&', MESSAGE_COMMON['UNIT']);
+        $('.descriptions').append('<p class=\"description-' + index + '\">' + msg + '</p>');
+      });
+
+      pieParts = pieParts.toString().substring(1, pieParts.length);
+      $('.report').find('.pie').text(pieParts);
+      this.chart.peity('pie');
+    } else {
+      $('.report').find('.peity').detach();
+      $('.report').find('.pie').detach();
+      $('.descriptions').detach().html('<p class=\"description-0\">' + MESSAGE_COMMON['NOTHING'] + '</p>').prependTo('.report');
+    }
   },
   reportData: '',
   getData: function() {
@@ -67,9 +89,7 @@ var Report = {
       Report.renderToPage();
     },
     error: function() {
-      var errorMessage = ERROR.ERROR_GET_DATA_FAILED;
-
-      $('.error').text(errorMessage);
+      $('.error').text(ERROR.ERROR_GET_DATA_FAILED);
       $('.report').hide();
     }
     });
@@ -82,10 +102,10 @@ var Report = {
     try{
       chrome.storage.sync.get(mydata, function(item) {
         var data = item[mydata];
-        if(!!data) {
+        //if(!!data) {
           Report.reportData = data;
           Report.renderToPage();
-        }
+        //}
       });
     } catch(e) {
       console.log('nodata');
@@ -96,10 +116,10 @@ var Report = {
 
 $('#clear_history').click(function() {
   chrome.storage.sync.clear();
-  alert('数据都清除了，让我们重头开始吧');
+  alert(MESSAGE_COMMON['RESET']);
+  Report.init();
 });
 
 $(document).ready(function() {
   Report.init();
-
 });
