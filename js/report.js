127307bbef00b5d5997678516f356a98b2ec9884
mocovenwitch <mocovenwitch@gmail.com>
2014-03-14 19:32:27 +0800

    localization-1

--- js/report.js
+++ js/report.js
@@ -16,48 +16,20 @@ $.fn.peity.defaults.pie = {
   width: 300
 }
 
-var MESSAGE_DESCRIPTION = {
-  'music': '花了 @@ &&听音乐，你真的可以好好做事吗？骗鬼把。',
-  'game': '好吧，你花了 @@ &&打游戏，需要我提醒你，打游戏找不到女朋友吗……',
-  'eshop': '逛网店 @@ &&，信用卡透支的滋味，好好尝尝吧。',
-  'sns': '社交网络占据了你 @@ &&，真的这么寂寞吗？',
-  'video': '总共花了 @@ &&看韩剧、美剧、日剧、中剧，你导演的吗？看得这么有劲。',
-  'news': '新闻需要看 @@ &&吗？赶紧做事去吧。',
-  'mocoven': '谢谢访问默考文的主页，但是用不着花 @@ &&吧',
-  'search': '你在搜索什么？需要花掉 @@ &&',
-  'study': '看起来好像花了 @@ &&在学习，不过效果怎么样，谁知道呢',
-  'other':'你访问了一些乱七八糟的网站，鬼知道干了些什么，只知道你花了 @@ &&！'
-};
 
-var MESSAGE_COMMON = {
-    'RESET': '数据都清除了，让我们重头开始吧',
-    'TITLE': '看看你都干了些什么！',
-    'APP_DESCRIPTION': '有一些计划做了好多年了，比如学一种弦乐；有一些决心下了 50 遍了，比如<br>「如果我再把学英文的时间拿去逛淘宝，'
-                       + '就剁手！」<br>「如果我再把学『解剖学』的时间拿去刷微博，就不准吃三文鱼刺身！」<br><br>结果呢，5 年过去了，说好的学弦乐呢？信用卡账单也把眼球吓掉了吧？'
-                       + '那你的手剁了吗？至于解剖学……你能立刻说出窦房结在哪里以及它的功能吗？<br><br>所以，好好看看，你都干了些什么吧。',
-    'UNIT': '分钟',
-    'COMMENTS': '亲爱的，你就好好的，滚去学习吧',
-    'NOTHING': '你还什么都没有干呢……',
-    'NOTE': '这个应用不会收集你的浏览记录，不要担心我会把他们发给你的女朋友/男朋友。<br>如果你想重头开始观察自己有没有好好学习，'
-            + '请点击绿色按钮「我要洗心革面」。<br><br>另外，如果发现自己一直在虚度人生，竟然每天花了 8 小时在刷微博，请不要太过悔恨，'
-            + '就怪这个应用的算法不合理好了。',
-    'RESET_BUTTON': '我要洗心革面'
-}
-
-var ERROR = {
-  ERROR_GET_DATA_FAILED: '数据库好像坏了，没有拿到数据……重新下载应用吧……不好意思'
-};
 
 //types: eshop/sns/music/video/game/news/other
 
 var Report = {
   chart: $('span.pie'),
   init: function() {
+    $(document).attr('title', MESSAGE_COMMON['TITLE']);
     $('#app_description').html(MESSAGE_COMMON['APP_DESCRIPTION']);
     $('.title').text(MESSAGE_COMMON['TITLE']);
     //$('.comments').text(MESSAGE_COMMON['COMMENTS']);
     $('.note').html(MESSAGE_COMMON['NOTE']);
     $('#clear_history').text(MESSAGE_COMMON['RESET_BUTTON']);
+    $('.mocoven_website').text(MESSAGE_COMMON['MOCOVEN_WEBSITE']);
 
     this.getData();
   },
